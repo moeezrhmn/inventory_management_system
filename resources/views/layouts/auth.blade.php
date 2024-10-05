99371016acee26ba5d42428f5e2df2a478ffc47d
baabaylog <baabaylog@gmail.com>
2024-10-05 13:35:39 +0500

    updates and speed optimize

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -55,7 +55,7 @@
             <div class="container container-tight py-4">
                 <div class="text-center mb-4">
                     <a href="{{ url('/') }}" class="navbar-brand navbar-brand-autodark">
-                        <img src="{{ asset('static/logo.svg') }}" width="110" height="32" alt="Tabler" class="navbar-brand-image">
+                        <img src="{{ asset('static/ads-logo.jpg') }}" style="height: 130px;" width="210px" height="32" alt="Tabler" class="navbar-brand-image">
                     </a>
                 </div>
 
--- resources/views/layouts/tabler.blade.php
+++ resources/views/layouts/tabler.blade.php
@@ -50,8 +50,7 @@
                 </button>
                 <h1 class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
                     <a href="{{ url('/') }}">
-                        <img src="{{ asset('static/logo.svg') }}" width="110" height="32" alt="Tabler"
-                            class="navbar-brand-image">
+                        <img src="{{ asset('static/ads-logo.jpg') }}" style="height: 50px;" width="110" height="32" alt="Tabler" class="navbar-brand-image">
                     </a>
                 </h1>
                 <div class="navbar-nav flex-row order-md-last">
