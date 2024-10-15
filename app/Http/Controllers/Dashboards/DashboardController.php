a8ef03cbc9a318fa1ba7355d4febbfbcbb33cb4b
baabaylog <baabaylog@gmail.com>
2024-10-15 08:30:20 +0500

    Most selling products

--- app/Http/Controllers/Dashboards/DashboardController.php
+++ app/Http/Controllers/Dashboards/DashboardController.php
@@ -10,6 +10,7 @@ use App\Models\Purchase;
 use App\Models\Quotation;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
@@ -30,6 +31,32 @@ class DashboardController extends Controller
         $quotations = Quotation::count();
         $quotationsAmountTotal = Quotation::sum('total_amount');
 
+        $mostSellingProducts = DB::query()
+        ->fromSub(function ($query) {
+            $query->from('order_details')
+                ->select('product_id', DB::raw('SUM(quantity) as total_quantity'))
+                ->groupBy('product_id')
+                ->unionAll(
+                    DB::table('quotation_details')
+                        ->select('product_id', DB::raw('SUM(quantity) as total_quantity'))
+                        ->groupBy('product_id')
+                );
+        }, 'combined_results')->limit(10)
+        ->join('products', 'combined_results.product_id', '=', 'products.id') // Join with products table
+        ->select('products.uuid', 'products.name', 'combined_results.product_id', DB::raw('SUM(total_quantity) as total_quantity'))
+        ->groupBy('combined_results.product_id', 'products.name', 'products.uuid')
+        ->orderBy('total_quantity', 'desc')
+        ->get();
+        $totalQuantity = $mostSellingProducts->sum('total_quantity');
+        
+        $mostSellingProducts = $mostSellingProducts->map(function ($product) use ($totalQuantity) {
+            $product->percentage = ($product->total_quantity / $totalQuantity) * 100;
+            return $product;
+        });
+        
+        // dd($mostSellingProducts);
+        
+
         return view('dashboard', [
             'products' => $products,
             'orders' => $orders,
@@ -42,7 +69,8 @@ class DashboardController extends Controller
             'todayOrders' => $todayOrders,
             'categories' => $categories,
             'quotations' => $quotations,
-            'quotationsAmountTotal' => $quotationsAmountTotal
+            'quotationsAmountTotal' => $quotationsAmountTotal,
+            'mostSellingProducts' => $mostSellingProducts,
         ]);
     }
 }
